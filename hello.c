72b5132bbf8de0ffd0e6312647a1f16bbd693c10
rldh1111 <rldh11111@naver.com>
2020-05-19 00:35:38 -0700

    add some files

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "myheader.h"
+
+void pinrtHello(void){
+	printf("Hello World!\n");
+}
---
+++ myheader.h
@@ -0,0 +1,8 @@
+#ifndef __MYHEADER_H__
+#define __MYHEADER_H__
+
+void pinrtHello(void);
+void printBye(void);
+
+#endif
+
